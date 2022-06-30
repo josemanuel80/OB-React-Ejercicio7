547841fd71f4bfe66bfbbfa78d82b390fb333aae
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2022-06-30 20:12:28 +0200

    Buttons added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { joke } from './model/joke.class';
 import axios from 'axios';
+import Button from '@mui/material/Button';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 
 function App() {
   const [data, setData] = useState('');
+  const [jokes, setJokes] = useState([]);
   const [like, setlike] = useState(false);
   const [dislike, setDislike] = useState(false);
   const newJoke = async () => {
@@ -17,10 +21,19 @@ function App() {
     newJoke();
   }, []);
 
-  console.log(data);
+  const handleClick = () => {
+    setJokes([...jokes, { id: data.id, like: like, dislike: dislike }]);
+    newJoke();
+  };
+  console.log(jokes);
   return (
     <div className="App">
       <p>{data.value}</p>
+      <Button variant="contained" onClick={handleClick}>
+        Joke
+      </Button>
+      <ThumbUpIcon />
+      <ThumbDownIcon />
     </div>
   );
 }

96ed7da298eb907e75bf1ac9f6df34855a250aae
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2022-06-30 20:01:24 +0200

    Showing jokes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { joke } from './model/joke.class';
+import axios from 'axios';
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState('');
+  const [like, setlike] = useState(false);
+  const [dislike, setDislike] = useState(false);
+  const newJoke = async () => {
+    return await axios
+      .get('https://api.chucknorris.io/jokes/random')
+      .then((e) => setData(e.data));
+  };
+
+  useEffect(() => {
+    newJoke();
+  }, []);
+
+  console.log(data);
+  return (
+    <div className="App">
+      <p>{data.value}</p>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/model/joke.class.js
@@ -0,0 +1,10 @@
+export class joke {
+  id = null;
+  like = false;
+  dislike = false;
+  constructor(id, like, dislike) {
+    this.id = id;
+    this.like = like;
+    this.dislike = dislike;
+  }
+}
